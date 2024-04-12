9f33b4333cece73a3ef6c090f8f8afa46ff17b8a
Shubham25123 <162818858+Shubham25123@users.noreply.github.com>
2024-04-12 08:14:36 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all Js code as newer version
+
+//alert(3+3) // we are using nodeJs, not browser
+console.log(3+3);
+console.log("shubham");
+
+let name = "shubham"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// PRIMITIVE DATA TYPE
+//number => 2 to power 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => standAlone value
+//undefined =>
+//symbol => unique
+
+
+// object
+
+console.log(typeof "shubham");
+console.log(typeof name);
+console.log(typeof undifined);
+console.log(typeof null);
