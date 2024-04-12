bd8948d0dee14eaf84a28ecb2974db83652ae46f
Shubham25123 <162818858+Shubham25123@users.noreply.github.com>
2024-04-12 07:47:32 +0000

    variable

---
+++ 01_basics/o1_var.js
@@ -0,0 +1,10 @@
+const accountId = 145566
+let accountEmail = "shubham@google.com"
+var accountPassword = "1234"
+accountCity = "jaipur"
+//accountId = 2
+accountEmail = "shub@google"
+accountPassword = "24577"
+accountcity = "patna"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
