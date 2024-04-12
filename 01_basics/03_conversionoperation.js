ef73c1df419cd43f2a69d65705f3b13a0f8f680d
Shubham25123 <162818858+Shubham25123@users.noreply.github.com>
2024-04-12 10:18:58 +0000

    conversion of data type

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,27 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33"=> 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null - 0
+//undefined - NaN
+
+let isLog = ""
+
+let bool = Boolean(isLog);
+console.log(bool);
+
+// " " => false
+//"shubham" - true
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
