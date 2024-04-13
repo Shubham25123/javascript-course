b897ee9913aaa7fbffcfbe290b596facdc7ac69c
Shubham25123 <162818858+Shubham25123@users.noreply.github.com>
2024-04-13 09:25:45 +0000

    conversion

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,4 +1,4 @@
-let score = "33abc"
+/*let score = "33abc"
 
 console.log(typeof score);
 console.log(typeof(score));
@@ -25,3 +25,30 @@ let somenumber = 33
 let stringnumber = String(somenumber)
 console.log(stringnumber);
 console.log(typeof stringnumber);
+
+// operations*/
+
+let value = 3
+let negvalue = -value
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2%3);
+ let str1= "hello"
+ let str2 = " shubham"
+ let str3 = str1 + str2
+ console.log(str3);
+
+ console.log("1"+2);
+ console.log(1 + "2");
+ console.log("1" + 2 + 2);
+ console.log(1+2+"2");
+
+ console.log(+true);
+ console.log(+"");
+
+ let num1, num2, num3
+ num1=num2=num3=2+2
+
+ let gamecounter = 100
+ gamecounter++;
+ console.log(gamecounter);
